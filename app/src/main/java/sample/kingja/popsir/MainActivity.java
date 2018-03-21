6d6e4a85b3b9483abd7960ecacf8d898bd4afb5d
KingJA <kingjavip@gmail.com>
2018-03-21 17:25:40 +0800

    begin 1.2.0

--- app/src/main/java/sample/kingja/popsir/MainActivity.java
+++ app/src/main/java/sample/kingja/popsir/MainActivity.java
@@ -9,17 +9,44 @@ import android.widget.Toast;
 
 import com.kingja.popsir.ListPop;
 import com.kingja.popsir.PopConfig;
+import com.kingja.popsir.PopSpinner;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private PopSpinner spinner1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button btn = findViewById(R.id.btn);
+        PopConfig config = new PopConfig.Builder(this)
+                .setPopHeight(500)
+                .build();
+        final ListPop listPop = new ListPop(config)
+                .setAdapter(new PopAdapter(this, Arrays.asList("A", "B", "C", "D", "E", "F")))
+                .setOnItemClickListener(new ListPop.OnItemClickListener<String>() {
+                    @Override
+                    public void onItemClick(String item, int position) {
+                        spinner1.setSelectText(item);
+                    }
+                });
+
+        spinner1 = findViewById(R.id.spinner1);
+        spinner1.setOnSpinnerStatusChangedListener(new PopSpinner.OnSpinnerStatusChangedListener() {
+            @Override
+            public void setOnSpinnerStatusChanged(boolean opened) {
+                if (opened) {
+                    listPop.showAsDropDown(spinner1);
+                }else{
+                    listPop.dismiss();
+                }
+            }
+        });
     }
 
     public void openPop(View view) {
@@ -27,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
         PopConfig config = new PopConfig.Builder(this)
                 .setPopHeight(500)
                 .build();
-
         new ListPop(config)
                 .setAdapter(new PopAdapter(this, Arrays.asList("A", "B", "C", "D", "E", "F")))
                 .setOnItemClickListener(new ListPop.OnItemClickListener<String>() {
